5392879a547c26be7aaa27388207aceb4fac8f90
PEAKYCOMMAND <crigfr3@gmail.com>
2024-04-27 17:59:45 -0300

    update-link

--- src/js/main.js
+++ src/js/main.js
@@ -11,8 +11,8 @@ function CreateList() {
 }
 //cria link no header
 const newCreate = new CreateList;
-newCreate.create('Star Plus', '/src/lib/strp/strp.html');
-newCreate.create('ESPN', '/src/lib/esp/esp.html');
-newCreate.create('Premiere', '/src/lib/pmr/pmr.html');
-newCreate.create('TNT', '/src/lib/lib/tnt.html');
-newCreate.create('Sport TV', '/src/lib/tnt/tnt.html');
+newCreate.create('Star Plus', './src/lib/strp/strp.html');
+newCreate.create('ESPN', './src/lib/esp/esp.html');
+newCreate.create('Premiere', './src/lib/pmr/pmr.html');
+newCreate.create('TNT', './src/lib/tnt/tnt.html');
+newCreate.create('Sport TV', './src/lib/tnt/tnt.html');
