e082746796295b850afcb2cac7dbd885032af480
PEAKYCOMMAND <crigfr3@gmail.com>
2024-04-28 08:53:46 -0300

    botão lista de canais

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,20 @@ newCreate.create('ESPN', './src/lib/esp/esp.html');
 newCreate.create('Premiere', './src/lib/pmr/pmr.html');
 newCreate.create('TNT', './src/lib/tnt/tnt.html');
 newCreate.create('Sport TV', './src/lib/tnt/tnt.html');
+
+//channel list
+const chList = document.querySelector('#btn-two')
+const responsiveUl = document.querySelector('#navigation-ul')
+
+chList.addEventListener('click', () => {
+  responsiveUl.style.left = '0px'
+ const btnExit = document.createElement('button')
+ btnExit.classList.add('btn-exit')
+ responsiveUl.appendChild(btnExit)
+
+ const btnExitA = document.createElement('a')
+ btnExit.appendChild(btnExitA)
+ btnExitA.classList.add('btn-exit-a')
+ btnExitA.innerText = 'X'
+
+})
